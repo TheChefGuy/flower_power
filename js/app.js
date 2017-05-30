3b8ff8e265787b82ff22308886435f0a1ea23e20
SoonJooUh <madeforonebox@gmail.com>
2017-05-30 15:15:18 -0700

    Add collision detection with axis-aligned bounding box

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
-
 // Enemies our player must avoid
 var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
+
+    // Variables applied to each instance 
     this.x = x;
     this.y = y;
+
+    // Random speed generator
     this.speed = Math.floor((Math.random() * 4) + 1);
+    
+    // Enemy collision area
     this.width = 50;
     this.height = 50;
 };
@@ -17,9 +18,8 @@ var Enemy = function(x,y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply any movement by the dt parameter to ensure
+    // the game runs at the same speed for all computers.
     this.x = this.x + 101 * dt * this.speed;
 
     // If the bug goes off of the board, reset its position and randomize the multiplier
@@ -37,8 +37,8 @@ Enemy.prototype.update = function(dt) {
 function drawBox(x, y, width, height, color) {
     ctx.beginPath();
     ctx.rect(x, y, width, height);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = color;
+    ctx.lineWidth = "";
+    ctx.strokeStyle = "";
     ctx.stroke();
 };
 
@@ -52,43 +52,81 @@ Enemy.prototype.reset = function() {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // draws boxes around enemy objects
-    drawBox(this.x, this.y + 77, 100, 67, "yellow");
+    
+    // Draws boxes around enemy objects
+    // Helped to understand box collision method
+    // drawBox(this.x, this.y + 77, 100, 67, "");
 };
 
 // Collisions - this is bound to an instance of the enemy
 Enemy.prototype.collisions = function() {
     var enemyBox = {
+        // Variables applied to each instance 
         x: this.x,
         y: this.y,
+        // Enemy collision area
         width: this.width,
         height: this.height
     };
     var playerBox = {
+        // Variables applied to each instance 
         x: player.x,
         y: player.y,
+        // Enemy collision area
         width: player.width,
         height: player.height
     };
+
     if (enemyBox.x < playerBox.x + playerBox.width &&
         enemyBox.x + enemyBox.width > playerBox.x &&
         enemyBox.y < playerBox.y + playerBox.height &&
         enemyBox.height + enemyBox.y > playerBox.y) {
-        // collision detected!
-        //console.log("Collision is detected.");
+        
+        // Collision detected!
+        console.log("Collision is detected.");
         player.collision();
     }
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 var Player = function(x,y) {
+    // Load player image
     this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
+    // Starting coordinates
+    this.x = 200;
+    this.y = 400;
+    // Player collision area
+    this.width = 50;
+    this.height = 50;
 };
 
+// Player reset method
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+// Player update method
+Player.prototype.update = function() {
+    if (this.y < 5) { // if player reaches the water
+        this.reset(); // position resets
+    }
+};
+
+// Player collision method
+Player.prototype.collision = function() {
+    player.reset();
+};
+
+// Player render method
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    
+    // draws a box around player object
+    // drawBox(this.x + 16, this.y + 62, 70, 78, "blue");
+};
+
+// Player input handler
 Player.prototype.handleInput = function(key) {
     switch (key) {
         case "left":
@@ -103,50 +141,38 @@ Player.prototype.handleInput = function(key) {
             break;
         case "up":
             if (this.y > 0) {
-                this.y -= 100;
+                this.y -= 85;
             }
             break;
         case "down":
             if (this.y < 400) {
-                this.y += 100;
+                this.y += 85;
             }
             break;
     }
 };
 
-Player.prototype.reset = function() {
-    this.x = 200;
-    this.y = 400;
-};
-
-Player.prototype.update = function() {
-    if (this.y < 5) { // if player reaches the water
-        this.reset(); // position resets
-    }
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [];
+// Y values for enemy starting
 var yVals = [220, 140, 60];
+// Random speed generator to create new speed for new enemy
+// Controls number of enemies on screen
 for (var i = 0; i < 6; i++) {
     var x = Math.floor((Math.random() * -1000) + 1);
     var y = yVals[Math.floor((Math.random() * 3))];
     enemy = new Enemy(x, y);
+    // Place new enemy in allEnemies array
     allEnemies.push(enemy);
 };
 
-
-player = new Player(200, 400);
+// Starting player object in a variable called player
+player = new Player();
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
